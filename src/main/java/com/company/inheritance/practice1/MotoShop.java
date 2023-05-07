37a4ec164e1905889d57757aa746fe8279f5024a
magdalenahuget <magdalena.huget@gmail.com>
2023-05-07 17:59:54 +0200

    Add motoShop excercise

---
+++ src/main/java/com/company/inheritance/practice1/MotoShop.java
@@ -0,0 +1,11 @@
+package com.company.inheritance.practice1;
+
+class MotoShop {
+    public static void main(String[] args) {
+        Tire tire = new Tire(123, "SuperTire", "ProContact", "Winter pro", 17, 225);
+        tire.printInfo();
+
+        ExhaustPart exhaustPart = new ExhaustPart(987, "Noisy Parts", "Turbo Brum", "Noisy Exhaust", true);
+        exhaustPart.printInfo();
+    }
+}
