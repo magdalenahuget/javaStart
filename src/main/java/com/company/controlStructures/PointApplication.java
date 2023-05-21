8a9f722d57af9a767015d5102c82037724450e0d
magdalenahuget <magdalena.huget@gmail.com>
2023-05-21 16:49:13 +0200

    Add control structures

---
+++ src/main/java/com/company/controlStructures/PointApplication.java
@@ -0,0 +1,19 @@
+package com.company.controlStructures;
+
+public class PointApplication {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(10, 20);
+        PointController pc = new PointController();
+
+        System.out.println("Punkt: (" + p1.getX() + ";" + p1.getY() + ")");
+        pc.addX(p1);
+        System.out.println("Punkt addX: (" + p1.getX() + ";" + p1.getY() + ")");
+        pc.addY(p1);
+        System.out.println("Punkt addY: (" + p1.getX() + ";" + p1.getY() + ")");
+        pc.minusX(p1);
+        System.out.println("Punkt minusX: (" + p1.getX() + ";" + p1.getY() + ")");
+        pc.minusY(p1);
+        System.out.println("Punkt minusY: (" + p1.getX() + ";" + p1.getY() + ")");
+    }
+}
---
+++ src/main/java/com/company/controlStructures/PointController.java
@@ -0,0 +1,19 @@
+package com.company.controlStructures;
+
+public class PointController {
+    public void addX(Point p) {
+        p.setX(p.getX() + 1);
+    }
+
+    public void minusX(Point p) {
+        p.setX(p.getX() - 1);
+    }
+
+    public void addY(Point p) {
+        p.setY(p.getY() + 1);
+    }
+
+    public void minusY(Point p) {
+        p.setY(p.getY() - 1);
+    }
+}
