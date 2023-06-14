9063ee2584930b0bbb97a36edaedd9f1a7b4e33c
magdalenahuget <magdalena.huget@gmail.com>
2023-06-14 17:53:16 +0200

    Initial commit with project structure

---
+++ src/main/java/com/company/filesReadingAndWriting/CompanyApp.java
@@ -0,0 +1,8 @@
+package com.company.filesReadingAndWriting;
+
+public class CompanyApp {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/company/filesReadingAndWriting/Employee.java
@@ -0,0 +1,4 @@
+package com.company.filesReadingAndWriting;
+
+public class Employee {
+}
