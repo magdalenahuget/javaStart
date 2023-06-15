75d78c8b4d69df44b312ebd94694134b5fb02f36
magdalenahuget <magdalena.huget@gmail.com>
2023-06-15 14:12:48 +0200

    Add racursion

---
+++ src/main/java/com/company/recursion/Factorial.java
@@ -0,0 +1,17 @@
+package com.company.recursion;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+
+        //0! = 1
+        //3! = 1 * 2 * 3
+        int factorialArgument = 3;
+        int result = factorial(factorialArgument);
+        System.out.println("Silnia " + factorialArgument + " = " + result);
+    }
+
+    static int factorial(int n) {
+        return n > 1 ? n * factorial(n - 1) : 1;
+    }
+}
