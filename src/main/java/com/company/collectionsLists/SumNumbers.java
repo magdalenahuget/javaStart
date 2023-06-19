c764bade27a0ad0543df0c72a41af1a69cac8864
magdalenahuget <magdalena.huget@gmail.com>
2023-06-19 11:15:13 +0200

    Add solution without list

--- src/main/java/com/company/collectionsLists/SumNumbers.java
+++ src/main/java/com/company/collectionsLists/SumNumbers.java
@@ -1,4 +1,32 @@
 package com.company.collectionsLists;
 
+import java.util.Objects;
+import java.util.Scanner;
+
+
 public class SumNumbers {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        collectNumbers();
+    }
+
+    public static void collectNumbers() {
+        String input;
+        int sum = 0;
+        StringBuilder turboString = new StringBuilder();
+
+        System.out.println("Hello, please provide numbers to sum. If you provided all, please type 'exit'.");
+        do {
+            System.out.println("Please provide the number or type 'exit' ");
+            input = scanner.nextLine();
+
+            if (Objects.equals(input, "exit")) {
+                break;
+            }
+            sum += Integer.parseInt(input);
+            turboString.append(input).append(" + ");
+        } while (true);
+        System.out.println(turboString + " = " + sum);
+    }
 }
