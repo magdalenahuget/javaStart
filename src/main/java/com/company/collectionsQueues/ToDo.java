768b0cd4abc13154a55662087ae382fe5ce4b3e8
magdalenahuget <magdalena.huget@gmail.com>
2023-06-30 17:15:40 +0200

    Add todo app

--- src/main/java/com/company/collectionsQueues/ToDo.java
+++
@@ -1,4 +0,0 @@
-package com.company.collectionsQueues;
-
-public class ToDo {
-}
---
+++ src/main/java/com/company/collectionsQueues/ToDoApp.java
@@ -0,0 +1,9 @@
+package com.company.collectionsQueues;
+
+public class ToDoApp {
+    public static void main(String[] args) {
+
+        TaskController taskController = new TaskController();
+        taskController.handleToDoList();
+    }
+}
