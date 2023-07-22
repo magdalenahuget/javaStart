6bb025fb7ad12abe06e8e5335fefe95c0414ead1
magdalenahuget <magdalena.huget@gmail.com>
2023-07-22 19:15:51 +0200

    Add streams practice

--- src/main/java/com/company/streams/StreamsTraining.java
+++ src/main/java/com/company/streams/StreamsTraining.java
@@ -1,4 +1,19 @@
 package com.company.streams;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class StreamsTraining {
+    public static void main(String[] args) {
+
+
+        int[] numbers = IntStream.iterate(0, x -> x + 1)
+                .filter(x -> x > 100 && x < 1000 && x % 5 == 0)
+                .limit(11)
+                .map(x -> x * 3)
+                .toArray();
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
 }
